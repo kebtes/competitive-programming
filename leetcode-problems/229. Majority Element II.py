02a1c802086206fd48d8ebc0c05dbf6e13b38974
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2024-02-09 22:13:02 +0300

    more problems

--- leetcode-problems/229. Majority Element II.py
+++ leetcode-problems/229. Majority Element II.py
@@ -1,12 +1,10 @@
 from collections import Counter
-class Solution:
-    def majorityElement(self, nums):
-        counted = Counter(nums)
-        compare = len(nums//3)
-        output = []
 
-        for num, count in counted.items():
-            if count > compare:
-                output.append(num)
-                
-        return output
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        counted = Counter(nums)
+
+        return [x for x,y in counted.items() if y > (len(nums)//3)]
+
+
+        
