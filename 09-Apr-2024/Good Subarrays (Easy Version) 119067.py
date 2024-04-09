45557fb8f990e675d2c8a0cd00d15398a2d984b7
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-04-09 13:47:49 +0300

    Created 'Good Subarrays (Easy Version)' by A2SV Hub

---
+++ 09-Apr-2024/Good Subarrays (Easy Version) 119067.py
@@ -0,0 +1,22 @@
+# Problem: Good Subarrays (Easy Version) - https://codeforces.com/problemset/problem/1736/C1
+
+N = int(input())
+
+for _ in range(N):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    
+    l=0
+    j=1
+
+    cnt=0
+    
+    for r in range(n):
+        while l < n and j > arr[r]:
+            l += 1
+            j -= 1
+
+        cnt += r - l + 1
+        j+=1
+
+    print(cnt)
