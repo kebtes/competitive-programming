8675abf7dd33128a46da9c5827e00738cc3132cf
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-04-17 18:17:43 +0300

    Created 'Middle of a Linked List' by A2SV Hub

---
+++ 17-Apr-2024/Middle of a Linked List 125872.py
@@ -0,0 +1,22 @@
+# Problem: Middle of a Linked List - https://leetcode.com/problems/middle-of-the-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]):
+        lst = []
+        curr = head
+        cnt = 0
+
+        while curr:
+            curr = curr.next
+            cnt += 1
+
+        for _ in range(cnt//2):
+            head = head.next
+
+        return head
+        
