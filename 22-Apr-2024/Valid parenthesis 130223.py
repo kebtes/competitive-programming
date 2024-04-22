63a666466fadca02928898f4268bcb185345cbba
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-04-22 19:25:29 +0300

    Created 'Valid parenthesis' by A2SV Hub

---
+++ 22-Apr-2024/Valid parenthesis 130223.py
@@ -0,0 +1,23 @@
+# Problem: Valid parenthesis - https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        closing = {")": "(", "}": "{", "]": "["}
+        opening = [char for char in closing.values()]
+        stack = []
+
+        for i in s:
+            if i in opening:
+                stack.append(i)
+            
+            else:
+                if len(stack) == 0:
+                    return False
+                
+                if closing[i] != stack.pop():
+                    return False
+            
+            
+        return len(stack) == 0
+                
