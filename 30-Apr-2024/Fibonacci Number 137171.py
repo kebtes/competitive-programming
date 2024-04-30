bc5424a3898fa2ef84a5c0c6b36d8d153bae3ae3
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-04-30 19:18:01 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 30-Apr-2024/Fibonacci Number 137171.py
@@ -0,0 +1,11 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        tb = [0,1]
+
+        for i in range(2, n+1):
+            tb.append(tb[i-1] + tb[i-2])
+        
+        return tb[n]
+        
