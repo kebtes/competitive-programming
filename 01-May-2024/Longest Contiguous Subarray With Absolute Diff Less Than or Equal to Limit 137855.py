2d6d6ec3ab2e2dcad53812f1f7996e1c7e8d4dda
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-05-01 09:55:29 +0300

    Created 'Longest Contiguous Subarray With Absolute Diff Less Than or Equal to Limit' by A2SV Hub

---
+++ 01-May-2024/Longest Contiguous Subarray With Absolute Diff Less Than or Equal to Limit 137855.py
@@ -0,0 +1,30 @@
+# Problem: Longest Contiguous Subarray With Absolute Diff Less Than or Equal to Limit - https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
+
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        increasing_queue = deque()
+        decreasing_queue = deque()
+        
+        l = 0
+        longest_length = 0
+
+        for r in range(len(nums)):
+            while increasing_queue and nums[r] > increasing_queue[-1]:
+                increasing_queue.pop()
+            increasing_queue.append(nums[r])
+
+            while decreasing_queue and nums[r] < decreasing_queue[-1]:
+                decreasing_queue.pop()
+            decreasing_queue.append(nums[r])
+
+            if increasing_queue[0] - decreasing_queue[0] > limit:
+                if increasing_queue[0] == nums[l]:
+                    increasing_queue.popleft()
+                if decreasing_queue[0] == nums[l]:
+                    decreasing_queue.popleft()
+                
+                l += 1
+            
+            longest_length = max(longest_length, r - l + 1)
+            
+        return longest_length
