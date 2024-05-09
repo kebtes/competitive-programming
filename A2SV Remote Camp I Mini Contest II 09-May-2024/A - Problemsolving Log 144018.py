6a044a1d9a85b55c131fa684006f80db88cfb0ce
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-05-09 13:16:40 +0300

    Created 'A - Problemsolving Log' by A2SV Hub

---
+++ A2SV Remote Camp I Mini Contest II 09-May-2024/A - Problemsolving Log 144018.py
@@ -0,0 +1,18 @@
+# Problem: A - Problemsolving Log - https://codeforces.com/gym/522997/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+
+    cnt = [0] * 26
+
+    for i in range(n):
+        cnt[ord(s[i]) - ord('A')] += 1
+    output = 0
+
+    for i in range(26):
+        if cnt[i] >= i + 1:
+            output += 1
+
+    print(output)
+
