68a3ac8c746cd64fb08b993f73bf50b6d14d04bd
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-05-14 21:00:04 +0300

    Created 'Team Composition: Programmers and Mathematicians' by A2SV Hub

---
+++ 14-May-2024/Team Composition: Programmers and Mathematicians 150147.py
@@ -0,0 +1,5 @@
+# Problem: Team Composition: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    print(min(min(a,b), (a+b) // 4))
