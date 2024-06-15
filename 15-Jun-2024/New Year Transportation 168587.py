729b6a1ce1c3fc842cc018ce058cc8536e0ed7e7
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-06-15 14:29:08 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 15-Jun-2024/New Year Transportation 168587.py
@@ -0,0 +1,22 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+from itertools import accumulate
+
+n, t = map(int, input().split())
+doors = list(map(int, input().split()))
+
+cur_door = 1
+
+for i in range(n):
+    if (cur_door) == t:
+        print("YES")
+        exit()
+    if (cur_door) > t:
+        print("NO")
+        exit()
+
+    if cur_door == (i + 1):
+        cur_door += doors[i]
+
+
+     
