d09f2b758862f63af83e5787c4f5f6bc3100929f
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-07-27 12:51:05 +0300

    Created 'Milena and Admirer' by A2SV Hub

---
+++ 27-Jul-2024/Milena and Admirer 185161.py
@@ -0,0 +1,17 @@
+# Problem: Milena and Admirer - https://codeforces.com/problemset/problem/1898/B
+
+for _ in range(int(input())):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    
+    op = 0
+
+    for i in range(n - 2, -1, -1):
+        if nums[i] > nums[i+1]:
+            parts = (nums[i] + nums[i+1] - 1) // nums[i+1]
+            new_value = nums[i] // parts
+            op += parts - 1
+            nums[i] = new_value
+
+    print(op)
+
