043c10247c34d0e23a1e89c96a278a6634cf91e2
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2024-10-09 17:35:28 +0300

    Created 'Sort List' by A2SV Hub

---
+++ 09-Oct-2024/Sort List 219022.py
@@ -0,0 +1,29 @@
+# Problem: Sort List - https://leetcode.com/problems/sort-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        dummy = ListNode(0)
+        dummy.next = head
+        curr = dummy.next
+
+        lst = []
+        while curr:
+            lst.append(curr.val)
+            curr = curr.next
+
+        lst.sort()
+
+        curr = dummy.next
+        for v in lst:
+            curr.val = v
+            curr = curr.next
+
+        return dummy.next
+            
+        
